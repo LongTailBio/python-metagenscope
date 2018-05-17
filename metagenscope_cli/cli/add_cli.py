f6330ddfa47a8a5355a52abf1fa983d26100aefa
David Danko <dcdanko@gmail.com>
2018-05-17 15:12:07 -0400

    add commands to manage groups

---
+++ metagenscope_cli/cli/add_cli.py
@@ -0,0 +1,77 @@
+import os
+import click
+from requests.exceptions import HTTPError
+
+from metagenscope_cli.network.authenticator import Authenticator
+from metagenscope_cli.config import config
+
+from .utils import add_authorization
+
+
+@click.group()
+def create():
+    pass
+
+
+@create.command(name='group')
+@add_authorization()
+@click.option('--private/--public', default=True)
+@click.argument('group_name')
+@click.argument('admin_email')
+def create_group(uploader, private, group_name, admin_email):
+    """Create a new organization."""
+    payload = {
+        'name': group_name,
+        'admin_email': admin_email,
+    }
+    if private:
+        payload['access_scheme'] = 'private'
+    try:
+        response = uploader.knex.post('/api/v1/organizations', payload)
+        click.echo(response)
+    except HTTPError as exc:
+        print(f'{exc}', file=stderr)
+
+
+@click.group()
+def add():
+    pass
+
+
+@add.command(name='group-to-org')
+@add_authorization()
+@click.argument('group_name')
+@click.argument('org_uuid')
+def add_group_to_org(uploader, group_name, org_uuid):
+    """Add a group to an organization."""
+    response = uploader.knex.get(f'/api/v1/sample_groups/getid/{group_name}')
+    group_uuid = response['data']['sample_group_uuid']
+    payload = {'sample_group_uuid': group_uuid}
+    try:
+        response = uploader.knex.post(
+            f'/api/v1/organizations/{org_uuid}/sample_groups',
+            payload
+        )
+        click.echo(response)
+    except HTTPError as exc:
+        print(f'{exc}', file=stderr)
+
+
+@add.command(name='user-to-org')
+@add_authorization()
+@click.argument('group_name')
+@click.argument('org_uuid')
+def add_group_to_org(uploader, group_name, org_uuid):
+    """Add a group to an organization."""
+    response = uploader.knex.get(f'/api/v1/sample_groups/getid/{group_name}')
+    group_uuid = response['data']['sample_group_uuid']
+    payload = {'sample_group_uuid': group_uuid}
+    try:
+        response = uploader.knex.post(
+            f'/api/v1/organizations/{org_uuid}/sample_groups',
+            payload
+        )
+        click.echo(response)
+    except HTTPError as exc:
+        print(f'{exc}', file=stderr)
+
